f9af4493e32077ce545a34a6deabafdf0b78466f
Ederas229 <120315229+Ederas229@users.noreply.github.com>
2024-03-23 19:07:20 +0100

    Initial Release

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,63 @@
-Hooks.once('init', async function() {
+let waitingRoll = false;
 
+Hooks.on("renderChatMessage", async function (message, html) {
+  if (message.isCheckRoll) {
+    html
+      .find(".message-buttons")
+      .append(
+        '<button type="button" class="triple-critical-success" data-action="tripleCritical" data-message="' +
+          message.id +
+          '">Critical 3x</button>'
+      )
+      .find(".triple-critical-success")
+      .on("click", { message: message, html: html }, rollTripleDamage);
+  }
 });
 
-Hooks.once('ready', async function() {
-
+Hooks.on("preCreateChatMessage", async function (message) {
+  if (!(message.isDamageRoll && waitingRoll)) return;
+  message.updateSource({ "flags.pf2e-crit-multi.ignoreDsN": true });
 });
+
+Hooks.on("createChatMessage", async function (message) {
+  if (message.isDamageRoll && waitingRoll) {
+    waitingRoll = false;
+    changeMulti(message);
+  }
+});
+
+Hooks.on("diceSoNiceRollStart", (messageId, context) => {
+  //Hide this roll
+  const message = game.messages.contents.find((e) => e.id == messageId);
+  if (!message.getFlag("pf2e-crit-multi", "ignoreDsN")) return;
+  context.blind = true;
+});
+
+async function rollTripleDamage(event) {
+  const message = event.data.message;
+  waitingRoll = true;
+  event.data.html.find(".critical-success").trigger("click");
+}
+
+async function changeMulti(message) {
+  const DamageRoll = CONFIG.Dice.rolls.find(
+    (Roll) => Roll.name === "DamageRoll"
+  );
+
+  ui.chat.element.find(message.id).remove();
+  ChatMessage.deleteDocuments([message.id]);
+  const roll = new DamageRoll(
+    message.rolls[0]._formula.replaceAll("2 *", "3 *")
+  );
+  await roll.evaluate();
+
+  message.flags["pf2e-crit-multi"].ignoreDsN = false;
+  ChatMessage.create({
+    flavor: message.flavor,
+    content: roll.total,
+    type: message.type,
+    speaker: message.speaker,
+    flags: message.flags,
+    rolls: [roll],
+  });
+}
